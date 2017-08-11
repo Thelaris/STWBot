70a0d4b474abdfe6da096a754c6fe21d0d56c610
Thelaris <kurt.widdup@gmail.com>
2017-08-11 18:59:12 +1000

    refactor(pug role): updates to pug role setting
    Still not working

--- STWBot/UserEvents.cs
+++ STWBot/UserEvents.cs
@@ -33,7 +33,7 @@ namespace STWBot
 
 		static public void bot_UserJoined(object user, Discord.UserEventArgs e)
 		{
-			//System.Threading.Thread.Sleep(3000);
+			System.Threading.Thread.Sleep(1000);
 
 			/* Conflicting
 			if (e.User.Roles.Count() < 2)
@@ -54,8 +54,10 @@ namespace STWBot
 		static public void bot_UserUpdated(object user, Discord.UserUpdatedEventArgs e)
 		{
 			string usertype = "";
+			System.Threading.Thread.Sleep(1000);
 			if (e.After.Roles.Count() < 2)
 			{
+				
 				SetRole(user, e);
 			}
 			if (usertype != "" && usertype != "0" && usertype != "1")
@@ -164,19 +166,21 @@ namespace STWBot
 			Discord.Channel chan = e.Server.FindChannels("__").FirstOrDefault();
 			Discord.Channel mainChannel = e.Server.FindChannels("Sum Ting Wong￬").FirstOrDefault();
 			//string userType = "1";
+			Discord.Role pugsRole = e.Server.FindRoles("PUGS").FirstOrDefault();
+
+
+
 			if (e.After.VoiceChannel == null) return;
 			if (e.After.VoiceChannel == chan)
 			{
 
-				Discord.Role pugsRole = e.Server.FindRoles("PUGS").FirstOrDefault();
-
 				//e.After.AddRoles(pugsRole);
-
-				//System.Threading.Thread.Sleep(1000);
 
 				//e.Server.FindRoles("PUGS").FirstOrDefault();
 
 				List<Discord.Role> roles = new List<Discord.Role>(new Discord.Role[] { e.Server.FindRoles("PUGS").FirstOrDefault() });
+
+				//System.Threading.Thread.Sleep(500);
 
 				e.After.Edit(voiceChannel: mainChannel, roles: roles);
 
@@ -185,6 +189,9 @@ namespace STWBot
 			}
 			else
 			{
+				System.Threading.Thread.Sleep(500);
+				if (e.After.Roles.Equals(pugsRole)) return;
+				if (e.After.Roles.Count() > 0) return;
 				Discord.Role membersRole = e.Server.FindRoles("MEMBERS").FirstOrDefault();
 
 				e.After.AddRoles(membersRole);
